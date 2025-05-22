b1caefc81db98ec850dec4c9671cc3ca1bf4b5d6
Shree <you@example.com>
2025-05-22 22:00:57 +0530

    task2 added

---
+++ Task2.py
@@ -0,0 +1,8 @@
+import math
+n = int(input("Enter a number: "))
+square_root = math.sqrt(n)
+log = math.log(n)
+sine = math.sin(n)
+print("Square root: ", square_root)
+print("logarithm: ", log)
+print("Sine: ", sine)
