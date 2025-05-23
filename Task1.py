6e600a1f16c03cd346e01fe49d167a9d3b46bfc9
Shree <you@example.com>
2025-05-23 16:33:03 +0530

    task1 updated

--- Task1.py
+++ Task1.py
@@ -1,8 +1,8 @@
 def factorial(n):
-    fact = 1
-    for i in range(1, n+1):
-        fact= fact*i
-    return fact
+    if(n<2):
+        return 1
+    else:
+        return n * (factorial(n-1))
 n = int(input("Enter a number: "))
 result = factorial(n)
 print("Factorial of",n,"is:",result)
